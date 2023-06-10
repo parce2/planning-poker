ac6a0ca3fdb0dcae4338e0b5509c27e882eb3835
Fernando Monterroza <fernandomonterroza48@gmail.com>
2023-06-10 12:23:49 -0500

    fix: details button

--- src/app/components/atoms/button/button.component.ts
+++ src/app/components/atoms/button/button.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { AcceptedClass } from '@components/atoms/consts/type/button-class.type';
 @Component({
   selector: 'a-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
+
 export class ButtonComponent {
-  @Input() disabled: boolean = true;
+  @Input() disabled: boolean = false;
+  @Input() customClass: AcceptedClass = 'a-button a-button--primary';
 }
---
+++ src/app/components/atoms/consts/type/button-class.type.ts
@@ -0,0 +1,5 @@
+type AcceptedClass =
+'a-button a-button--primary' |
+ 'a-button a-button--outline';
+
+export { AcceptedClass };
