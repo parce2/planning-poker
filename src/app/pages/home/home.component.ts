0aeb321363e5cf67e884909cc2ad07ce72058e5e
Fernando Monterroza <fernandomonterroza48@gmail.com>
2023-06-12 09:54:45 -0500

    form delete

--- src/app/pages/home/home.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'home-selector',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-})
-export class HomeComponent { }
