e9b758333ca931be6fee7269f4bc04dfe3b4304e
Fernando Monterroza <fernandomonterroza48@gmail.com>
2023-06-12 09:57:10 -0500

    add backup

---
+++ src/app/components/atoms/card/card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'a-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent {
+
+}
